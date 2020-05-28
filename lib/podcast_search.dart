d0bbd70efea0fced764ad5cf50b84ed374861482
samisnotinsane <sameen11@hotmail.com>
2020-05-28 18:29:41 +0100

    Adding search autofill icon button.

--- lib/podcast_search.dart
+++ lib/podcast_search.dart
@@ -1,7 +1,21 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:podster_flutter/podcast.dart';
 
 class PodcastSearch extends SearchDelegate<Podcast> {
+  final titles = [
+    'Joe Rogan Experience',
+    'GABA',
+    'Monday Morning Podcast',
+    'The Intelligence',
+    'Naval',
+    'Today in Focus',
+    'Waveform: The MKBHD Podcast',
+    'POLITICO Dispatch'
+  ];
+
+  final recentTitles = ['Naval', 'GABA'];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
@@ -31,6 +45,23 @@ class PodcastSearch extends SearchDelegate<Podcast> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    final suggestions = query.isEmpty ? recentTitles : titles;
+    return ListView.builder(
+      itemCount: suggestions.length,
+      itemBuilder: (context, index) => ListTile(
+        title: Text(suggestions[index]),
+        trailing: Transform.rotate(
+          angle: 310 * math.pi / 180,
+          child: InkWell(
+            child: Icon(Icons.arrow_upward),
+            onTap: () {
+              query = suggestions[index];
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
