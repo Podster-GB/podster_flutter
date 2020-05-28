a56099f939651ed4e9ca413fce33b5c2ca07dffb
samisnotinsane <sameen11@hotmail.com>
2020-05-28 16:19:24 +0100

    Changing app title.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Podster',
+      title: 'Podster Alpha',
       theme: ThemeData(
         primaryColor: PRIMARY_COLOR,
         accentColor: ACCENT_COLOR,
