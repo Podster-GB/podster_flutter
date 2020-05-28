64291e20eff63a0ff8dbe41b7395fbc3d25cf5f6
samisnotinsane <sameen11@hotmail.com>
2020-05-28 16:43:55 +0100

    Revert "Changing app title."
    This reverts commit a56099f939651ed4e9ca413fce33b5c2ca07dffb.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Podster Alpha',
+      title: 'Podster',
       theme: ThemeData(
         primaryColor: PRIMARY_COLOR,
         accentColor: ACCENT_COLOR,
