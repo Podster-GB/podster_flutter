0d5303ff4a736e4c42730d32d53e13f2b62b67ab
samisnotinsane <sameen11@hotmail.com>
2020-07-14 13:15:51 +0100

    Add player placeholder and control btns.

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(
+  printer: PrettyPrinter(
+      methodCount: 1, // number of method calls to be displayed
+      errorMethodCount: 8, // number of method calls if stacktrace is provided
+      lineLength: 30, // width of the output
+      colors: true, // Colorful log messages
+      printEmojis: false, // Print an emoji for each log message
+      printTime: true // Should each log print contain a timestamp
+      ),
+);
 
 class PlayerScreen extends StatelessWidget {
   static const String id = '/player';
@@ -6,9 +18,42 @@ class PlayerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Player'),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 9,
+              child: Placeholder(),
+            ),
+            Flexible(
+              child: ControlButtons(),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ControlButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        IconButton(
+          onPressed: () => null,
+          icon: Icon(Icons.replay_10),
+        ),
+        IconButton(
+          onPressed: () => null,
+          icon: Icon(Icons.play_arrow),
+        ),
+        IconButton(
+          onPressed: () => null,
+          icon: Icon(Icons.forward_10),
+        ),
+      ],
     );
   }
 }
